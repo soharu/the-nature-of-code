18f86f8e624279a4555f36e28c50c918f9f82d5e
Jahyun Oh <soharu1024@gmail.com>
2018-11-06 20:55:30 +0900

    Add 8-directional random walker

--- random-walks/sketch.js
+++ random-walks/sketch.js
@@ -1,11 +1,11 @@
-class Walker {
+class Walker4D {
   constructor(x, y) {
     this.x = x;
     this.y = y;
   }
 
   display() {
-    stroke(0);
+    stroke('rgba(100%, 0%, 0%, 0.2)');
     point(this.x, this.y);
   }
 
@@ -13,11 +13,9 @@ class Walker {
 
     var x = this.x;
     var y = this.y;
-    // var stepSize = 3;
 
     while (x == this.x && y == this.y) {
       var choice = floor(random(0, 4));
-
       if (choice == 0) {
         x = min(this.x + 1, 640); // right
       } else if (choice == 1) {
@@ -34,7 +32,36 @@ class Walker {
   }
 }
 
-var walker = new Walker(50, 50);
+class Walker9D {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  display() {
+    stroke('rgba(0%, 0%, 100%, 0.2)');
+    point(this.x, this.y);
+  }
+
+  step() {
+
+    var x = this.x;
+    var y = this.y;
+
+    while (x == this.x && y == this.y) {
+      var stepX = floor(random(0, 3)) - 1;
+      var stepY = floor(random(0, 3)) - 1;
+      x = min(this.x + stepX, 640);
+      y = min(this.y + stepY, 360);
+    }
+
+    this.x = x;
+    this.y = y;
+  }
+}
+
+var walker1 = new Walker4D(320, 180);
+var walker2 = new Walker9D(320, 180);
 
 function setup() {
   createCanvas(640, 360);
@@ -42,6 +69,8 @@ function setup() {
 }
 
 function draw() {
-  walker.display();
-  walker.step();
+  walker1.display();
+  walker1.step();
+  walker2.display();
+  walker2.step();
 }
